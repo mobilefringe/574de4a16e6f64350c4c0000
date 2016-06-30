32bfe34c59cb8bbb167989fc77bd0656f70ae2f9
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2016-06-30 11:47:19 -0400

    Seacliff Village 1.0  /site.js

--- site.js
+++ site.js
@@ -2,5 +2,5 @@ var site_json = {
                     "name" : "Seacliff Village",
                     "default_image" : "http://assets.codecloudapp.com/sites/574de4a16e6f64350c4c0000/image/jpeg/undefined/sv_500x500_logo.jpg",
                     "time_zone" : "T08:00:00Z",
-                    "social_feed" : "http://seacliff.mallmaverick.com/api/v2/seacliff/social.json"
+                    "social_feed" : "http://pacificcommons.mallmaverick.com/api/v2/pacificcommons/social.json"
                 }
