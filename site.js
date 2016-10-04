1cd47ff7f92d01e3be10e1006566a4f0c39416db
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-10-04 17:21:12 -0400

    Seacliff Village 1.1  /site.js

---
+++ site.js
@@ -0,0 +1,6 @@
+var site_json = {
+                    "name" : "Seacliff Village",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/57f418bb6e6f6449746d0000/image/jpeg/undefined/sv_500x500_logo.jpg",
+                    "time_zone" : "T08:00:00Z",
+                    "social_feed" : "//seacliff.mallmaverick.com/api/v2/seacliff/social.json"
+                }
